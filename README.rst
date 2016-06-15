dcaae8d05ff15abab009631dedf4c5a73f5ecdbc
Thiago Souza <thiago@elastic.co>
2016-06-15 15:17:09 +0000

    initial commit

---
+++ README.rst
@@ -0,0 +1,9 @@
+Pyngboard
+==========
+
+This project provides a python library for accessing `Pingboard <http://docs.pingboard.apiary.io>`_ resources.
+
+License
+========
+
+This project and it's documentation is released under the `MIT License <https://opensource.org/licenses/MIT>`_.
---
+++ setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import re
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# Get the version
+version_regex = r'__version__ = ["\']([^"\']*)["\']'
+with open('requests_oauthlib/__init__.py', 'r') as f:
+    text = f.read()
+    match = re.search(version_regex, text)
+
+    if match:
+        VERSION = match.group(1)
+    else:
+        raise RuntimeError("No version number found!")
+
+
+APP_NAME = 'pyngboard'
+
+settings = dict()
+
+
+# Publish Helper.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+tests_require = ['mock', 'requests-mock']
+if sys.version_info < (2, 7): # Python 2.6 or lower
+    tests_require.append('unittest2')
+
+
+settings.update(
+    name=APP_NAME,
+    version=VERSION,
+    description='A Pingboard Python library',
+    long_description=open('README.rst').read(),
+    author='Thiago Souza',
+    author_email='thiago@elastic.co',
+    url='https://github.com/tsouza/pyngboard',
+    packages=['pyngboard'],
+    install_requires=['requests_oauthlib>=0.6.2', 'requests>=2.0.0'],
+    license='MIT',
+    classifiers=(
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ),
+    zip_safe=False,
+    tests_require=tests_require,
+    test_suite='tests'
+)
+
+setup(**settings)
