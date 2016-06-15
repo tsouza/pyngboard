a4c514476b0af602e29278a887dab04f2bb72a37
Thiago Souza <thiago@elastic.co>
2016-06-15 17:10:49 +0000

    initial credentials implementation

--- pyngboard/__init__.py
+++ pyngboard/__init__.py
@@ -1,1 +1,11 @@
-VERSION="0.0.1"
+__version__ = '0.0.1'
+
+import logging
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+   class NullHandler(logging.Handler):
+       def emit(self, record):
+           pass
+
+logging.getLogger('pyngboard').addHandler(NullHandler())
---
+++ pyngboard/auth/args.py
@@ -0,0 +1,8 @@
+class ArgsCredentials:
+
+    def __init__(self, **kwargs):
+        self.client_id = kwargs['client_id']
+        self.client_secret = kwargs['client_secret']
+
+    def __load():
+        return client_id != null && client_secret != null;    
